43861d7c9243a5d13778ddce04c03e6da2d8a6e0
Sevenannn <qianqliu@uw.edu>
2024-05-16 10:15:45 -0700

    fix

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -132,7 +132,7 @@ impl ChannelBuilder {
     }
 
     async fn create_client(&self) -> Result<SparkSession, Error> {
-        let endpoint = format!("http://{}:{}", self.host, self.port);
+        let mut endpoint = format!("http://{}:{}", self.host, self.port);
 
         if (self.use_ssl) {
             endpoint = format!("https://{}:{}", self.host, self.port);
